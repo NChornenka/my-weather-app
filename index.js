e7df6d84e5a6fd575dd8b1f1fbd5457648088754
NChornenka <88328438+NChornenka@users.noreply.github.com>
2022-10-22 16:27:28 -0400

    Added degrees conversion to forecast

--- index.js
+++ index.js
@@ -1,12 +1,102 @@
 
-  function searchCity(cityName) {
+  function getCity(event){
+      event.preventDefault();
+      let cityName = document.querySelector("#input").value;
+      if (cityName === ""){
+        alert("Please enter city name")
+      }
+      searchCity(cityName); 
+  }
+
+function searchCity(cityName) {
   let unit = "metric";
   let apiKey = "d682095eed5d0971ffeaf2a35ab9e196";
   let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather?q=";
   let apiUrl = `${apiEndPoint}${cityName}&appid=${apiKey}&units=${unit}`;
   axios.get(apiUrl).then(showTemperature);
+  axios.get(apiUrl).then(getMetricForecast);
 
 }
+function showTemperature(response){
+  celciusTemperature = response.data.main.temp;
+  feelsLikeTemperature = response.data.main.feels_like;
+  windSpeed = response.data.wind.speed;
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(celciusTemperature);
+  document.querySelector("#feels-like").innerHTML = Math.round(feelsLikeTemperature);
+  document.querySelector("#condition").innerHTML = response.data.weather[0].main;
+  document.querySelector("#wind").innerHTML = Math.round(windSpeed);
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  let degreeIndicatorElement = document.querySelector("#degree-indicator");
+  degreeIndicatorElement.innerHTML = "C";
+  let windSpeedIndicatorElement = document.querySelector("#wind-speed-indicator");
+  windSpeedIndicatorElement.innerHTML = "m/s";
+  document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+  let dateElement = document.querySelector("#date-element");
+  let timeElement = document.querySelector("#time-element");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  timeElement.innerHTML = formatTime(response.data.dt * 1000);
+  getMetricForecast();
+  
+  }
+  function getMetricForecast(){
+    let cityName = document.getElementById("city").innerHTML;
+    let units = "metric";
+    let apiKey = "80e0f9db4e77fe70fa2t2749330ddo5a";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${cityName}&key=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(displayMetricForecast);
+  }
+
+  function displayMetricForecast(response){
+    let forecastElement = document.querySelector("#forecast");
+    let forecast = response.data.daily;
+    let forecastHTML = "";
+    forecast.forEach(function(forecastDay) {
+      forecastHTML = forecastHTML + `        
+      <div class="row text-center align-items-center">
+      <div class="col-4 weather-forecast-day">
+        ${formatDay(forecastDay.time)}
+      </div>
+      <div class="col-4">
+        <img id="icon" src=${forecastDay.condition.icon_url} width="50">
+      </div>
+      <div class="col-4">
+        <span class="max-temperature">${Math.round(forecastDay.temperature.maximum)}°</span><span class="min-temperature"> ${Math.round(forecastDay.temperature.minimum)}°</span>
+      </div>
+    </div>`;
+    })
+    forecastElement.innerHTML = forecastHTML;
+  }
+
+  function getImperialForecast(){
+    let cityName = document.getElementById("city").innerHTML;
+    let units = "imperial";
+    let apiKey = "80e0f9db4e77fe70fa2t2749330ddo5a";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${cityName}&key=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(displayImperialForecast);
+  }
+
+  function displayImperialForecast(response){
+    let forecastElement = document.querySelector("#forecast");
+    let forecast = response.data.daily;
+    let forecastHTML = "";
+    forecast.forEach(function(forecastDay) {
+      forecastHTML = forecastHTML + `        
+      <div class="row text-center align-items-center">
+      <div class="col-4 weather-forecast-day">
+        ${formatDay(forecastDay.time)}
+      </div>
+      <div class="col-4">
+        <img id="icon" src=${forecastDay.condition.icon_url} width="50">
+      </div>
+      <div class="col-4">
+        <span class="max-temperature">${Math.round(forecastDay.temperature.maximum)}°</span><span class="min-temperature"> ${Math.round(forecastDay.temperature.minimum)}°</span>
+      </div>
+    </div>`;
+    })
+    forecastElement.innerHTML = forecastHTML;
+
+  }
 
   function showPosition(event){
   event.preventDefault();
@@ -22,45 +112,11 @@
   axios.get(apiUrl).then(showTemperature);
   }
 
-  function showTemperature(response){
-  celciusTemperature = response.data.main.temp;
-  feelsLikeTemperature = response.data.main.feels_like;
-  windSpeed = response.data.wind.speed;
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(celciusTemperature);
-  document.querySelector("#feels-like").innerHTML = Math.round(feelsLikeTemperature);
-  document.querySelector("#condition").innerHTML = response.data.weather[0].main;
-  document.querySelector("#wind").innerHTML = Math.round(windSpeed);
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
-  let dateElement = document.querySelector("#date-element");
-  let timeElement = document.querySelector("#time-element");
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  timeElement.innerHTML = formatTime(response.data.dt * 1000);
-  
-  getForecast(response.data.name);
-  
-}
-  function getForecast(name){
-    let units = "metric";
-    let apiKey = "80e0f9db4e77fe70fa2t2749330ddo5a";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${name}&key=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(displayForecast);
-  }
-
-  function showWeather(event){
-    event.preventDefault();
-    let cityName = document.querySelector("#input").value;
-    if (cityName === ""){
-      alert("Please enter city name")
-    }
-    searchCity(cityName); 
-    }
-
   function intoFarenheits(event){
   event.preventDefault();
     celcius.classList.add("active");
     farenheits.classList.remove("active");
+
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round((celciusTemperature * 9)/5 + 32);
     let feelLikeElement = document.querySelector("#feels-like");
@@ -71,6 +127,9 @@
     windSpeedElement.innerHTML = Math.round(windSpeed * 2.237);
     let windSpeedIndicatorElement = document.querySelector("#wind-speed-indicator");
     windSpeedIndicatorElement.innerHTML = "mph";
+
+    getImperialForecast();
+
   }
 
 function intoCelcius(event){
@@ -87,6 +146,8 @@ function intoCelcius(event){
   windSpeedElement.innerHTML = Math.round(windSpeed);
   let windSpeedIndicatorElement = document.querySelector("#wind-speed-indicator");
   windSpeedIndicatorElement.innerHTML = "m/s";
+
+  getMetricForecast();
 }
 
 function formatDate(timestamp){
@@ -116,31 +177,12 @@ function formatDay(timestamp){
 
 }
 
-function displayForecast(response){
-  let forecastElement = document.querySelector("#forecast");
-  let forecast = response.data.daily;
-  let forecastHTML = "";
-  forecast.forEach(function(forecastDay) {
-    forecastHTML = forecastHTML + `        
-    <div class="row text-center align-items-center">
-    <div class="col-4 weather-forecast-day">
-      ${formatDay(forecastDay.time)}
-    </div>
-    <div class="col-4">
-      <img id="icon" src=${forecastDay.condition.icon_url} width="50">
-    </div>
-    <div class="col-4">
-      <span class="max-temperature">${Math.round(forecastDay.temperature.maximum)}°</span><span class="min-temperature"> ${Math.round(forecastDay.temperature.minimum)}°</span>
-    </div>
-  </div>`;
-  })
-  forecastElement.innerHTML = forecastHTML;
-}
+
 
 
 
 let search = document.querySelector("#submit");
-search.addEventListener("click", showWeather);
+search.addEventListener("click", getCity);
 
 let celciusTemperature = null;
 let feelsLikeTemperature = null;
